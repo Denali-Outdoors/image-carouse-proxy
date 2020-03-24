7eef658d57b835e4f51cfba98b457b222bc2c018
EclecticTaco <calvi5210@gmail.com>
2020-03-24 11:45:03 -0700

    add reservations and reviews component

--- server/index.js
+++ server/index.js
@@ -3,12 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log('Hello from proxy server!')
-})
-
 app.use(express.static(path.join(__dirname, '/../public')));
 
-app.listen(3002, () => {
-  console.log('Server listening on port 3002');
+app.listen(3010, () => {
+  console.log('Server listening on port 3010');
 });
